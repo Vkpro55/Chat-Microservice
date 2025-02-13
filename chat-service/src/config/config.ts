0177d305022844875e29c5960f670d3a51d4479b
Vinod Kumar <vinodrao835@gmail.com>
2025-02-13 08:41:39 +0530

    FEAT: Added config file for typescript and db

--- chat-service/src/config/config.ts
+++ chat-service/src/config/config.ts
@@ -1,1 +1,20 @@
   
+import { config } from "dotenv";
+
+const configFile = `./.env`;
+config({ path: configFile });
+
+const { MONGO_PASS, MONGO_USERNAME, PORT, JWT_SECRET, NODE_ENV, MESSAGE_BROKER_URL } =
+    process.env;
+
+const queue = { notifications: "NOTIFICATIONS" };
+
+export default {
+    MONGO_PASS,
+    MONGO_USERNAME, 
+    PORT,
+    JWT_SECRET,
+    env: NODE_ENV,
+    msgBrokerURL: MESSAGE_BROKER_URL,
+    queue,
+};
